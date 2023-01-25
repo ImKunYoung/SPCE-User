05b68cece316414a68214af4df2a4bc07930b133
ImKunYoung <ur2kunyoung2@outlook.com>
2023-01-25 13:38:09 +0900

    chore: Update Config file

--- src/main/java/com/example/spceuser/SpceUserApplication.java
+++ src/main/java/com/example/spceuser/SpceUserApplication.java
@@ -2,8 +2,10 @@ package com.example.spceuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpceUserApplication {
 
     public static void main(String[] args) {
