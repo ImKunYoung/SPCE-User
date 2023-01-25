92b144709174c4b8b76553c688e3cbbc53395181
ImKunYoung <ur2kunyoung2@outlook.com>
2023-01-25 13:34:09 +0900

    initial commit

---
+++ src/main/java/com/example/spceuser/SpceUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.spceuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpceUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpceUserApplication.class, args);
+    }
+
+}
